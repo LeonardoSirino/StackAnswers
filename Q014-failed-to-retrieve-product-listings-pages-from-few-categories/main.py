8312d7c40961fa5c37bb2f45a2f8865416b95250
Leonardo Sirino <leonardosirino@outlook.com>
2022-01-08 11:10:32 -0300

    Solutio proposal to Q014

--- Q014-failed-to-retrieve-product-listings-pages-from-few-categories/main.py
+++ Q014-failed-to-retrieve-product-listings-pages-from-few-categories/main.py
@@ -4,10 +4,6 @@ import scrapy
 class NorgrenSpider(scrapy.Spider):
     name = 'norgren'
     start_urls = ['https://www.norgren.com/de/en/list']
-    custom_settings = {
-        'LOG_FILE': 'norgren.log',
-        'LOG_ENABLE': False,
-    }
 
     def start_requests(self):
         for start_url in self.start_urls:
@@ -15,13 +11,19 @@ class NorgrenSpider(scrapy.Spider):
 
     def parse(self, response):
         link_list = []
-        for item in response.css(".match-height a.more-info::attr(href)").getall():
+
+        more_info_items = response.css(
+            ".match-height a.more-info::attr(href)").getall()
+
+        detail_items = [item for item in more_info_items if '/detail/' in item]
+        if len(detail_items) > 0:
+            print(f'This is a link you are searching for: {response.url}')
+
+        for item in more_info_items:
             if not "/detail/" in item:
                 inner_page_link = response.urljoin(item)
-                print(f"Found link: {inner_page_link}")
                 link_list.append(inner_page_link)
                 yield {"target_url": inner_page_link}
 
         for new_link in link_list:
             yield scrapy.Request(new_link, callback=self.parse)
-
